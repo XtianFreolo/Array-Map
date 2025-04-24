356061ac4b814d5533d6b09da6789f163884d37e
XtianFreolo <f60@umbc.edu>
2025-04-24 00:06:14 -0400

    This embarks my Maps activity

---
+++ Starter Code-2/users.js
@@ -0,0 +1,8 @@
+alert("Its working Sire");
+
+
+const users = [
+	{firstName: "Alice", lastName: "Johnson", points: 120},
+	{firstName: "Bob", lastName: "Smith", points: 99},
+	{firstName: "Charlie", lastName: "Brown", points: 180}
+];
