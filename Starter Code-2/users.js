c6330c097d1a4286e1e7c458aad25c222c1f2ecd
XtianFreolo <f60@umbc.edu>
2025-04-24 00:29:41 -0400

    clear assignment

--- Starter Code-2/users.js
+++ Starter Code-2/users.js
@@ -6,3 +6,13 @@ const users = [
 	{firstName: "Bob", lastName: "Smith", points: 99},
 	{firstName: "Charlie", lastName: "Brown", points: 180}
 ];
+
+const updatedUsers = users.map(user => {
+	return {
+		fullname: `${user.firstName} ${user.lastName}`, 
+		membershipStatus: user.points > 100 ? "Premium" : "Standard"
+	};
+
+});
+
+console.log(updatedUsers);
